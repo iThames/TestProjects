6af8d22dea51e9cff489aafcc32858a1d0683ece
muppaneni <krishna.precise@gmail.com>
2018-03-30 14:06:35 +0100

    selenium3 and gecko driver statements added

--- LearnSelenium/src/SeleniumTests/FirstSeleniumTest.java
+++ LearnSelenium/src/SeleniumTests/FirstSeleniumTest.java
@@ -5,6 +5,12 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstSeleniumTest {
 	public static void main(String[] args) {
+		
+		String projectLocation = System.getProperty("user.dir");
+		
+		System.setProperty("webdriver.gecko.driver", projectLocation + "/lib/geckodriver/geckodriver");
+		System.setProperty("webdriver.firefox.bin", "/Applications/Firefox.app/Contents/MacOS/firefox-bin");
+		
 		WebDriver driver = new FirefoxDriver();
 		
 		driver.get("http://seleniumhq.org/");
