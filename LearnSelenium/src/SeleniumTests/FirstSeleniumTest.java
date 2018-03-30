8dd5e51daf7fa3eb63700d650ed45f2e90c69152
muppaneni <krishna.precise@gmail.com>
2018-03-30 18:11:29 +0100

    added chromedriver jar and statements

--- LearnSelenium/src/SeleniumTests/FirstSeleniumTest.java
+++ LearnSelenium/src/SeleniumTests/FirstSeleniumTest.java
@@ -1,6 +1,7 @@
 package SeleniumTests;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstSeleniumTest {
@@ -8,10 +9,12 @@ public class FirstSeleniumTest {
 		
 		String projectLocation = System.getProperty("user.dir");
 		
-		System.setProperty("webdriver.gecko.driver", projectLocation + "/lib/geckodriver/geckodriver");
-		System.setProperty("webdriver.firefox.bin", "/Applications/Firefox.app/Contents/MacOS/firefox-bin");
+		//System.setProperty("webdriver.gecko.driver", projectLocation + "/lib/geckodriver/geckodriver");
+		//System.setProperty("webdriver.firefox.bin", "/Applications/Firefox.app/Contents/MacOS/firefox-bin");
+		//WebDriver driver = new FirefoxDriver();
 		
-		WebDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", projectLocation + "/lib/chromedriver/chromedriver");		
+		WebDriver driver = new ChromeDriver();
 		
 		driver.get("http://seleniumhq.org/");
 		driver.quit();
